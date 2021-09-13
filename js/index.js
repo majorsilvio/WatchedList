21a97f83fed9acf3544dad5b568f06edba2a89a9
Silvio Paiva <silvioej@gmail.com>
2021-09-13 11:51:14 -0300

    list container - country list

--- js/index.js
+++ js/index.js
@@ -1,52 +1,7 @@
-$(document).ready(() => {
-    actual = -1;
-    let rollTo;
-    rollDown();
+$(document).ready(function()
+{
+  $.get("view/list-container/list-container.html", function(html_string)
+   {
+      $('#list').html(html_string);
+   });
 });
-
-$('.page-up').on('click',() => rollUp());
-$('.page-down').on('click',() => rollDown());
-
-function rollDown() {
-    actual++;
-    rollTo = $('.container').children()[actual]?.id
-    rollTo ? roll() : actual--;
-}
-
-function rollUp() {
-    actual--
-    rollTo = $('.container').children()[actual] ? $('.container').children()[actual].id : actual = 0;
-    rollTo ? roll() : actual = 0;
-
-}
-
-function roll() {
-    actual <= 0 ? $('.page-up').addClass('hidden') : $('.page-up').removeClass('hidden')
-    if (rollTo) {
-        $('html, .container').animate({
-            scrollTop: $(`#${rollTo}`)?.offset()?.top
-        }, 'slow');
-    }
-
-}
-
-$('html,body').bind('mousewheel', function (e) {
-    e.stopImmediatePropagation()
-    e.stopPropagation();
-    if (e.originalEvent.wheelDelta / 120 > 0) {
-        rollUp();
-    } else {
-        rollDown();
-    }
-})
-
-$('html,body').bind('keyup', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    e.stopImmediatePropagation();
-    if (e.keyCode == '38') {
-        rollUp();
-    } else if (e.keyCode == '40') {
-        rollDown();
-    }
-})
---
+++ js/sectionScroll.js
@@ -0,0 +1,52 @@
+$(document).ready(() => {
+    actual = 0;
+    let rollTo;
+    rollDown();
+});
+
+$('.page-up').on('click',() => rollUp());
+$('.page-down').on('click',() => rollDown());
+
+function rollDown() {
+    actual++;
+    rollTo = $('.container').children()[actual]?.id
+    rollTo ? roll() : actual--;
+}
+
+function rollUp() {
+    actual--
+    rollTo = $('.container').children()[actual] ? $('.container').children()[actual].id : actual = 0;
+    rollTo ? roll() : actual = 0;
+
+}
+
+function roll() {
+    actual <= 0 ? $('.page-up').addClass('hidden') : $('.page-up').removeClass('hidden')
+    if (rollTo) {
+        $('html, .container').animate({
+            scrollTop: $(`#${rollTo}`)?.offset()?.top
+        }, 'slow');
+    }
+
+}
+
+$('html,body').bind('mousewheel', function (e) {
+    e.stopImmediatePropagation()
+    e.stopPropagation();
+    if (e.originalEvent.wheelDelta / 120 > 0) {
+        rollUp();
+    } else {
+        rollDown();
+    }
+})
+
+$('html,body').bind('keyup', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+    if (e.keyCode == '38') {
+        rollUp();
+    } else if (e.keyCode == '40') {
+        rollDown();
+    }
+})
